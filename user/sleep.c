bbe5a62c2be057d51968bf845c0b5eaf862b1e55
pvqn <phamvoquynhnhu24@gmail.com>
2024-02-24 22:25:35 +0700

    solution for project01

---
+++ user/sleep.c
@@ -0,0 +1,14 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user.h"
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(2, "Usage: sleep for a new sec...\n");
+        exit(-1);
+    }
+
+    sleep(atoi(argv[1]));
+
+    exit(0);
+}
